3a35ff461830b3a30356468db5e725b61411910f
6110110556 <6110110556@psu.ac.th>
2020-08-13 23:03:36 +0700

    part 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import WordCard from './WordCard';
 
+const word = "Hello";
 function App() {
   return (
     <div>
-      Hello World hhh
+      <WordCard value={word}/>
+      
     </div>
+    
   );
 }
 
