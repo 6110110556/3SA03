c144be0b5c2299c30311a476a60f5af912e78c92
6110110556 <6110110556@psu.ac.th>
2020-08-14 03:32:54 +0700

    Update01:GUI

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word = "Hello";
+const word = "Hello";// fix is word 1 2 3
 function App() {
   return (
-    <div>
-      <WordCard value={word}/>
-      
-    </div>
+    <body>
+       <center><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><h1>WELLCOM</h1><br></br></center>
+      {/* <center><img src="https://image.freepik.com/free-vector/cute-cat-kitten-family-cartoon-doodle-banner_40564-293.jpg" alt="cat" aria-setsize=""></img></center> */}
+      <div>
+        <h1> This is Shuffle word game !</h1>
+        <h1>We will test your English knowledge.</h1>
+        
+        1. "สวัสดี" ภาษาอังกฎษว่าอะไร ? <br></br>
+        <WordCard value={word}/>
+
+        2. "แมว" ภาษาอังกฎษว่าอะไร ? <br></br>
+        <WordCard value={word}/>
+
+        3. "วิ่ง" ภาษาอังกฎษว่าอะไร ? <br></br>
+        <WordCard value={word}/>
+        
+      </div>
+    </body>
     
   );
 }
